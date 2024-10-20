009c0042ef021ff2d2aa0229dcf8a9c3ee3c7a63
MaxwellHUBIV <65359664+MaxwellHUBIV@users.noreply.github.com>
2024-10-21 00:32:45 +0300

    Исправление задания 1

--- LabWork_1/Lab1.py
+++ LabWork_1/Lab1.py
@@ -1,9 +1,11 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-# TODO заменить значение пропущенного элемента средним арифметическим
-medium_number = numbers
-medium_number[4] = 0
-medium = sum(medium_number)/(len(medium_number))
-medium_number[4] = medium
+missed_number = 4
 
-print("Измененный список:", medium_number)
+numbers_summ = sum(numbers[:missed_number]) + sum(numbers[missed_number+1:])
+amount_of_numbers = len(numbers)
+average_number = numbers_summ / amount_of_numbers
+
+numbers[missed_number] = average_number
+
+print("Измененный список:", numbers)
