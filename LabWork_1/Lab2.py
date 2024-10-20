3f82473738dd222711c104cbef9a2f927fdf7d06
MaxwellHUBIV <65359664+MaxwellHUBIV@users.noreply.github.com>
2024-10-21 00:34:25 +0300

    Коррекция задания 2
    Масло масляное, согласен.

--- LabWork_1/Lab2.py
+++ LabWork_1/Lab2.py
@@ -2,5 +2,6 @@
 
 book_size = 25 * 50 * 100 * 4
 byte_disk_size = 1.44 * 1024**2
-count_books = round(byte_disk_size/book_size,0)
+count_books = byte_disk_size / book_size
+
 print("Количество книг, помещающихся на дискету:", int(count_books))
